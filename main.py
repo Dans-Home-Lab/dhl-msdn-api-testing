8c81f6bff598685e1832301443cd60d40467a75c
Dan M <dan.morgan1@gmail.com>
2022-02-15 13:37:09 -0600

    Static assets change

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+#Some static elements first. Not really part of the API but this is also where we can store the openapi spec.
 @app.get("/")
 async def root():
     file = open("./static/index.html")
-    return Response(content=file.read(), media_type="text/html")
+    r = Response(content=file.read(), media_type="text/html")
+    file.close
+    return r
 
-
-
-
+@app.get("/static/theme.css")
+async def root():
+    file = open("./static/theme.css")
+    r = Response(content=file.read(), media_type="text/css")
+    file.close
+    return r
