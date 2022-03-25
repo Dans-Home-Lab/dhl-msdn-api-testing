4822f072c95fa82a2240bee1fc73445f10aafbee
Daniel Morgan <dum000m@homeoffice.wal-mart.com>
2022-03-25 08:35:51 -0500

    Added some try..catch..finally

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 import os
 
 settings = {
-    'host': os.environ.get('DHL-API-ACCOUNT_HOST', '[]'),
+    'host': os.environ.get('DHL-API-ACCOUNT_HOST',),
     'master_key': os.environ.get('DHL-API-ACCOUNT_KEY'),
     'database_id': os.environ.get('DHL-API-COSMOS_DATABASE'),
     'container_id': os.environ.get('DHL-API-COSMOS_CONTAINER')
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
+from models import db as DB
 
 app = FastAPI()
 
 #Some static elements first. Not really part of the API but this is also where we can store the openapi spec.
 @app.get("/")
 async def root():
-    file = open("./static/index.html")
-    r = Response(content=file.read(), media_type="text/html")
-    file.close
-    return r
+    try:
+        file = open("./static/index.html")
+        r = Response(content=file.read(), media_type="text/html")
+    finally:
+        file.close
+        return r
 
 @app.get("/static/theme.css")
 async def root():
-    file = open("./static/theme.css")
-    r = Response(content=file.read(), media_type="text/css")
-    file.close
-    return r
+    try:
+        file = open("./static/theme.css")
+        r = Response(content=file.read(), media_type="text/css")
+    finally:
+        file.close
+        return r
+
+    
+
+@app.get("/vpc/{id}")
+async def vpc_id(id):
+    DB.read_database
