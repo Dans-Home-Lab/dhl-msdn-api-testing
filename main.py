a96941a80416be6a39e9eef10148474a10121c73
Daniel Morgan <dum000m@homeoffice.wal-mart.com>
2022-03-31 10:33:19 -0500

    Added some models for database CRUD

--- main.py
+++ main.py
@@ -17,20 +17,28 @@ async def root():
         return r
 
 @app.get("/static/theme.css")
-async def root():
+async def theme():
     try:
         file = open("./static/theme.css")
-        r = Response(content=file.read(), media_type="text/css")
+        theme_css = Response(content=file.read(), media_type="text/css")
     finally:
         file.close
-        return r
+        return theme_css
 
-    
+@app.get("/db")
+async def get_all_databases():
+    databases = await DB.get_all_databases()
+    return databases
+
 
 @app.get("/db/{id}")
-async def vpc_id(id):
-    db_result = await DB.get_database(id)
+async def get_database(id):
+    db_result = await DB.get_database_path(id)
     if (db_result == NULL):
         raise HTTPException(status_code=404, detail="Item not found")
     return db_result
 
+@app.post("/db/{id}")
+async def create_database(id):
+    db_result = await DB.create_database(id)
+    return db_result
--- models/db.py
+++ models/db.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from fastapi import HTTPException
 from pydantic import BaseModel
 import azure.cosmos.cosmos_client as cosmos_client
 import azure.cosmos.exceptions as exceptions
@@ -10,7 +11,17 @@ DATABASE_ID = config.settings['database_id']
 
 dbclient = cosmos_client.CosmosClient(HOST,MASTER_KEY)
 
-async def get_database(id):
+async def get_all_databases():
+    print("Fetching all databases")
+    try:
+        databases = list(dbclient.list_databases())
+        if not databases:
+            return HTTPException(status_code=400, detail="No databases found")
+        return databases
+    except exceptions:
+        return HTTPException(status_code=500, detail="Server error occured.")
+
+async def get_database_path(id):
     print("Get a Database by id")
 
     try:
@@ -24,3 +35,15 @@ async def get_database(id):
         return NULL
         #from https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/cosmos/azure-cosmos/samples/database_management.py
         #Also see: https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/cosmos/azure-cosmos/samples/document_management.py#L41-L49
+
+async def create_database(id):
+    print('creating database by name: {0}'.format(id))
+    try:
+        dbclient.create_database(id=id)
+        print('Database with id \'{0}\' created'.format(id))
+        new_db_path = get_database_path(id)
+        return new_db_path
+    except exceptions.CosmosResourceExistsError:
+        print('Database with id \'{0}\' already exists'.format(id))
+        existing_db_path = get_database_path(id)
+        return HTTPException(status_code=309, detail="Location: {0}".format(existing_db_path))
